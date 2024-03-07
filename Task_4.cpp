f78162d34fa41dd43bfceebf78af6467cbc7708e
Lazu Igor <lazu.igor5356@gmail.com>
2024-03-07 09:37:52 +0300

    Task4+

--- Task_4.cpp
+++ Task_4.cpp
@@ -1,5 +1,35 @@
 #include <stdio.h>
 
+unsigned short inkr_un_short(unsigned short x){
+    int* v = reinterpret_cast<int*>(&x);
+    (*v)++;
+    return(*(reinterpret_cast<unsigned short*>(v)));
+}
+
+short inkr_short(short x){
+    int* v = reinterpret_cast<int*>(&x);
+    (*v)++;
+    return(*(reinterpret_cast<short*>(v)));
+}
+
+float inkr_float(float x){
+    int* v = reinterpret_cast<int*>(&x);
+    (*v)++;
+    return(*(reinterpret_cast<float*>(v)));
+}
+
+unsigned int inkr_un_int(unsigned int x){
+    int* v = reinterpret_cast<int*>(&x);
+    (*v)++;
+    return(*(reinterpret_cast<unsigned int*>(v)));
+}
+
+int inkr_int(int x){
+    int* v = reinterpret_cast<int*>(&x);
+    (*v)++;
+    return(*(reinterpret_cast<int*>(v)));
+}
+
 int main(int argc, char *argv[]){
 
     unsigned short m = 33;
@@ -13,16 +43,26 @@ int main(int argc, char *argv[]){
     int g = 2147483647;
     int h = -2147483648;
 
-    printf("m)%05hu ",++m);
-    printf("n)%05hi ",++n);
-    printf("a)%013.2f ",++a);
-    printf("b)%013.2f ",++b);
-    printf("c)%013.2f ",++c);
-    printf("d)%013.2f ",++d);
-    printf("e)%010u ",++e);
-    printf("f)%010u ",++f);
-    printf("g)%010i ",++g);
-    printf("h)%010i ",++h);
+    printf("m)%05hu ",m);
+    printf("%05hu \n",inkr_un_short(m));
+    printf("n)%05hi ",n);
+    printf("%05hi \n",inkr_short(n));
+    printf("a)%013.2f ",a);
+    printf("%013.2f \n",inkr_float(a));
+    printf("b)%013.2f ",b);
+    printf("%013.2f \n",inkr_float(b));
+    printf("c)%013.2f ",c);
+    printf("%013.2f \n",inkr_float(c));
+    printf("d)%013.2f ",d);
+    printf("%013.2f \n",inkr_float(d));
+    printf("e)%010u ",e);
+    printf("%010u \n",inkr_un_int(e));
+    printf("f)%010u ",f);
+    printf("%010u \n",inkr_un_int(f));
+    printf("g)%010i ",g);
+    printf("%010i \n",inkr_int(g));
+    printf("h)%010i ",h);
+    printf("%010i \n",inkr_int(h));
 
     return 0;
 
