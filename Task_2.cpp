ff9a5ee3dd8cc5a77d7b239372ae9a56797de51f
Merkulov Sergey <sergeinder@mail.ru>
2024-03-07 08:19:51 +0300

    Fix the messages

--- Task_2.cpp
+++ Task_2.cpp
@@ -1,47 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
-void print16(void *p){
-    printf("a)%04hx ",*(reinterpret_cast<unsigned short *>(p)));
-    //printf("b)%016hb ", *(reinterpret_cast<unsigned short *>(p)));
-    printf("c)%05hu ",*(reinterpret_cast<unsigned short *>(p)));
-    printf("d)%04hx ",*(reinterpret_cast<short *>(p)));
-    //printf("e)%016hb ", *(reinterpret_cast<short *>(p)));
-    printf("f)%05hi ",*(reinterpret_cast<short *>(p)));
-    printf("\n");
-}
-
-void print32(void *p){
-    printf("a)%08x ",*(reinterpret_cast<unsigned int*>(p)));
-    //printf("b)%032b ", *(reinterpret_cast<unsigned int *>(p)));
-    printf("c)%010u ",*(reinterpret_cast<unsigned int*>(p)));
-    printf("d)%08x ",*(reinterpret_cast<int*>(p)));
-    //printf("e)%032b ", *(reinterpret_cast<int *>(p)));
-    printf("f)%010i ",*(reinterpret_cast<int*>(p)));
-    printf("e)%05.2e ",*(reinterpret_cast<float*>(p)));
-    printf("g)%043.2f ",*(reinterpret_cast<float*>(p)));
-    
-
-    printf("\n");
-}
-
 void arifmetic(void *p){
     short two = 2;
     void* ptr_two = &two;
-    printf("Signed multiple by 2 %hd\n",(*(reinterpret_cast<short*>(p))) * (*(reinterpret_cast<short*>(ptr_two))));
-    printf("Unigned multiple by 2 %hu\n",(*(reinterpret_cast<unsigned short*>(p))) * (*(reinterpret_cast<unsigned short*>(ptr_two))));
-    printf("Signed division by 2 %hd\n",(*(reinterpret_cast<short*>(p))) / (*(reinterpret_cast<short*>(ptr_two))));
-    printf("Unigned division by 2 %hu\n",(*(reinterpret_cast<unsigned short*>(p))) / (*(reinterpret_cast<unsigned short*>(ptr_two))));
+    printf("Sign multiple by 2 %hd\n",(*(reinterpret_cast<short*>(p))) * (*(reinterpret_cast<short*>(ptr_two))));
+    printf("Unign multiple by 2 %hu\n",(*(reinterpret_cast<unsigned short*>(p))) * (*(reinterpret_cast<unsigned short*>(ptr_two))));
+    printf("Sign division by 2 %hd\n",(*(reinterpret_cast<short*>(p))) / (*(reinterpret_cast<short*>(ptr_two))));
+    printf("Unign division by 2 %hu\n",(*(reinterpret_cast<unsigned short*>(p))) / (*(reinterpret_cast<unsigned short*>(ptr_two))));
     short sixteen = 16;
     void* ptr_sexteen = &sixteen;
-    printf("Calculation of the remainder of an unsigned division by 16%hu\n",(*(reinterpret_cast<unsigned short*>(p))) % (*(reinterpret_cast<unsigned short*>(ptr_sexteen))));
-    printf("Rounding down to a multiple of 16%hu\n", (*(reinterpret_cast<unsigned short*>(p))) * (floor((*(reinterpret_cast<unsigned short*>(p))) / (*(reinterpret_cast<unsigned short*>(ptr_sexteen))))));
-    printf("SOME %hu\n", *(reinterpret_cast<unsigned short*>(p)) << 1);
-    printf("SOME %hd\n", *(reinterpret_cast<short*>(p)) << 1);
-    printf("SOME %hu\n", *(reinterpret_cast<unsigned short*>(p))>> 1);
-    printf("SOME %hd\n", *(reinterpret_cast<short*>(p)) >> 1);
-    printf("x & 15 %hd", *(reinterpret_cast<unsigned short*>(p)) & 15);
-    printf("x & 16 %hd", *(reinterpret_cast<unsigned short*>(p)) & 16);
+    printf("Calculation of the remainder of an unsigned division by 16 %hu\n",(*(reinterpret_cast<unsigned short*>(p))) % (*(reinterpret_cast<unsigned short*>(ptr_sexteen))));
+    printf("Rounding down to a multiple of 16 %hu\n", (*(reinterpret_cast<unsigned short*>(p))) * (floor((*(reinterpret_cast<unsigned short*>(p))) / (*(reinterpret_cast<unsigned short*>(ptr_sexteen))))));
+    printf("Sign shift to the left by 1 bit %hu\n", *(reinterpret_cast<short*>(p)) << 1);
+    printf("Unign shift to the left by 1 bit %hd\n", *(reinterpret_cast<unsigned short*>(p)) << 1);
+    printf("Sign shift to the left by 1 bit %hu\n", *(reinterpret_cast<short*>(p))>> 1);
+    printf("Unsign shift to the left by 1 bit %hd\n", *(reinterpret_cast<unsigned short*>(p)) >> 1);
+    printf("x & 15 %hd\n", *(reinterpret_cast<unsigned short*>(p)) & 15);
+    printf("x & -16 %hd\n", *(reinterpret_cast<unsigned short*>(p)) & -16);
 
 }
 
